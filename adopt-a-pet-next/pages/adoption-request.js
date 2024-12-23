db953f96c3d06326f6b505beaf3e21baf400a2ad
Vihaa-T <160472877+Vihaa-T@users.noreply.github.com>
2024-12-23 21:06:30 +0530

    Create adoption-request.js

---
+++ adopt-a-pet-next/pages/adoption-request.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdoptionRequestForm from '../components/AdoptionRequestForm';
+
+const AdoptionRequestPage = () => {
+  return (
+    <div>
+      <AdoptionRequestForm />
+    </div>
+  );
+};
+
+export default AdoptionRequestPage;
